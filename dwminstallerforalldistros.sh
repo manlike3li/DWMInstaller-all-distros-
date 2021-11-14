13ac9f805a233777d97b0063cf5e07ba3b0930a7
manlike3li <87651881+manlike3li@users.noreply.github.com>
2021-11-14 16:06:18 +0000

    Add files via upload

---
+++ dwminstallerforalldistros.sh
@@ -0,0 +1,16 @@
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/dmenu
+cd dwm
+make
+sudo make clean install
+cd ..
+cd st
+make
+sudo make clean install
+cd ..
+cd dmenu
+make
+sudo make clean install
+cd
+echo if there is an error, then you probably need dependences for X or need base-devel, git or make.
